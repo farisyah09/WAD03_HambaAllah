da1e32ca554c8538c7cb11982da665b78face365
MhmmdAbdullah <adho77798@gmail.com>
2025-09-20 18:16:34 +0700

    feat: update server.js to include aboutUs routes (fix)

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+//Import routes
+const aboutUsRoutes = require('./routes/aboutUsRoutes');
 
 // Base route
 app.get('/', (req, res) => {
