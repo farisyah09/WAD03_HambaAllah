2674d2d22dbb90ee7395b7d76816407025669d2b
muhamad-helmi-ramadhan <healme1405@gmail.com>
2025-10-13 20:47:44 +0700

    refactor : perbaikan shoppingCart

--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const cartController = require('../controller/cartController'); 
+const cartController = require('../controller/cartController');
+const authMiddleware = require('../middleware/authMiddleware');  
 
-router.get('/:username', cartController.getCart);
+router.get('/:username', cartController.getCartItems);
 
-router.post('/:username/add', cartController.onlyBuyerCanAccessCart, cartController.addItemToCart);
+router.post('/:username/add', authMiddleware.isBuyer, cartController.addItemToCart);
 
-router.post('/:username/remove', cartController.onlyBuyerCanAccessCart, cartController.removeItemFromCart);
+router.post('/:username/remove', authMiddleware.isBuyer, cartController.removeItemFromCart);
 
 module.exports = router;
